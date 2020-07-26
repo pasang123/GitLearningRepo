bdfcd329074b94050d7eed25f2d92a74effcc3ac
Pasang <pas.sherpa123@gmail.com>
2020-07-26 18:48:12 +0530

    branch commit

--- test3.py
+++ test3.py
@@ -1,21 +1,3 @@
-# class Singleton:
-#    __instance = None
-#    @staticmethod
-#    def getInstance():
-#       """ Static access method. """
-#       if Singleton.__instance == None:
-#          Singleton()
-#       return Singleton.__instance
-#    def __init__(self):
-#       """ Virtually private constructor. """
-#       if Singleton.__instance != None:
-#          raise Exception("This class is a singleton!")
-#       else:
-#          Singleton.__instance = self
-# s = Singleton()
-# print(s)
-# s1=Singleton()
-# print(s1)
 
 
 # print ([1,2]*2)
